e50563be783d0083006f526a4da1d2e137d8bcbb
matheus proença <matheusproenca2020@hotmail.com>
2023-10-06 17:34:23 -0300

    Arrumando o Middleware

--- app/backend/src/middleware/userValidation.ts
+++ app/backend/src/middleware/userValidation.ts
@@ -1,21 +1,21 @@
 import { RequestHandler } from 'express';
 
 class UserValidation {
-  static loginValidation:RequestHandler = (req, res, next): Response | void => {
+  static loginValidation:RequestHandler = (req, res, next) => {
     const { email, password } = req.body;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const test = emailRegex.test(email);
-    if (!email || typeof email !== 'string') {
-      res.status(400).json({ message: 'All fields must be filled' });
+    if (!email) {
+      return res.status(400).json({ message: 'All fields must be filled' });
     }
-    if (!password || typeof password !== 'string') {
-      res.status(400).json({ message: 'All fields must be filled' });
+    if (!password) {
+      return res.status(400).json({ message: 'All fields must be filled' });
     }
     if (!test) {
-      res.status(401).json({ message: 'Invalid email or password' });
+      return res.status(401).json({ message: 'Invalid email or password' });
     }
     if (password.length <= 6) {
-      res.status(401).json({ message: 'Invalid email or password' });
+      return res.status(401).json({ message: 'Invalid email or password' });
     }
     next();
   };
